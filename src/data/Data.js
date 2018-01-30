bace5491d5417295819a899a0983e58388d5cffb
Lam Ngo <lam.ngo.qu@gmail.com>
2018-01-30 21:54:22 +0200

    Modified data

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,14 +1,14 @@
 const playerList = [
-  {name: John Doe, age: 15, score: 23},
-  {name: Lam Ngo, age: 24, score: 32},
-  {name: Ada Lovelace, age: 47, score: 108},
-  {name: Grace Hopper, age: 86, score: 102},
-  {name: Linus Torvalds, age: 48, score: 91},
-  {name: Alan Turing, age: 42, score: 97},
-  {name: James Gosling, age: 62, score: 86},
-  {name: Guido van Rossum, age: 61, score: 74},
-  {name: Robert Martin, age: 66, score: 67},
-  {name: Matz Matsumoto, age: 52, score: 59},
+  {name: "John Doe", age: 15, score: 23},
+  {name: "Lam Ngo", age: 24, score: 32},
+  {name: "Ada Lovelace", age: 47, score: 108},
+  {name: "Grace Hopper", age: 86, score: 102},
+  {name: "Linus Torvalds", age: 48, score: 91},
+  {name: "Alan Turing", age: 42, score: 97},
+  {name: "James Gosling", age: 62, score: 86},
+  {name: "Guido van Rossum", age: 61, score: 74},
+  {name: "Robert Martin", age: 66, score: 67},
+  {name: "Matz Matsumoto", age: 52, score: 59},
 ];
 
 export default playerList;
