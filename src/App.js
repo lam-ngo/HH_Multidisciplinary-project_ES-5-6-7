8f0155c52d29be325a6c1af1610f89e0034170a7
Lam Ngo <lam.ngo.qu@gmail.com>
2018-01-30 22:49:12 +0200

    Define actionHandle for all actions in child components

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import PlayerListing from './components/listing/PlayerListing.js';
 import PlayerAdding from './components/adding/PlayerAdding.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      playerList: playerList,
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,10 +25,26 @@ class App extends Component {
           <h1 className="App-title">This is a player app using React and ES 5, 6, 7 features</h1>
         </header>
 
-        <PlayerAdding />
-        <PlayerListing playerList={playerList}/>
+        <PlayerAdding actionHandle={this.actionHandle}/>
+        <PlayerListing actionHandle={this.actionHandle} playerList={this.state.playerList}/>
       </div>
     );
+  }
+
+  //HANDLE ALL ACTION AND SETSTATE
+  actionHandle = (type, index, player) => {
+    switch (type) {
+      case 'PLAYER_ADD':
+        console.log(`PLAYER_ADD new ${player}`);
+        this.setState({ playerList: [ ...this.state.playerList, player] });
+        break;
+      case 'PLAYER_DELETE':
+        console.log(`PLAYER_DELETE index ${index}`);
+        this.setState({ playerList: [ ...this.state.slice(0,index), ...this.state.slice(index+1)] });
+        break;
+      default:
+        break;
+    }
   }
 }
 
